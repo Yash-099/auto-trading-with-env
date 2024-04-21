6f130d0ea32b09e0c1ebc021682c354281c95e93
Yash-099 <ybhavsar@adobe.com>
2024-04-22 00:26:26 +0530

    :feat filter out sellside which is already purged

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ Automating analysis(for now) and trading(far fetched goal)
 
 reference for getting data via tradingview
 https://www.youtube.com/watch?v=WhuB5cbr-kY
+
+Intervals
+
+in_1_minute = "1"
+in_3_minute = "3"
+in_5_minute = "5"
+in_15_minute = "15"
+in_30_minute = "30"
+in_45_minute = "45"
+in_1_hour = "1H"
+in_2_hour = "2H"
+in_3_hour = "3H"
+in_4_hour = "4H"
+in_daily = "1D"
+in_weekly = "1W"
+in_monthly = "1M"
--- analyse_nifty_50_stocks.py
+++ analyse_nifty_50_stocks.py
@@ -2,7 +2,7 @@ from analysis_utils import *
 from trade_utils import *
 from tvDatafeed import Interval
 from config import *
-
+import time
 
 if __name__=='__main__':
     data_agent = DataAgent()
@@ -10,10 +10,10 @@ if __name__=='__main__':
         data = data_agent.get_ohlc_data(i, Interval.in_weekly, 100)
 
         analysis_agent = AnalysisAgent()
-        sellsides = analysis_agent.find_sellside_liquidity(data, 10)
-        nearest_sellside = analysis_agent.get_if_near_sellside(data, 5, 3)
+        sellsides = analysis_agent.find_sellside_liquidity(data, 3)
+        nearest_sellside = analysis_agent.get_if_near_sellside(data, percentage=5, num_neighbours=3)
         nearest_sellside_price = nearest_sellside['nearest_liquidity_in_range']
 
         if nearest_sellside_price!=0:
             print(f'Stock {i} is near its liquidity pool at price {nearest_sellside_price} current price is {data[-1]["close"]}')
-        
+        time.sleep(10)
