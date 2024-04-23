701ef260a119c6281f903a347c14a05dd13f170d
Yash-099 <ybhavsar@adobe.com>
2024-04-24 00:26:31 +0530

    :feat adding first cut fvg detection both sibi and bisi

--- analyse_nifty_50_stocks.py
+++ analyse_nifty_50_stocks.py
@@ -3,19 +3,23 @@ from trade_utils import *
 from tvDatafeed import Interval
 from config import *
 import time
-
+from tqdm import tqdm
 if __name__=='__main__':
     data_agent = DataAgent()
-    for i in nifty_50_stocks:
-        retry_count = 3
+    file = open(f'daily_analysis/nifty_stocks/{date.today()}.txt', 'w')
+    file.close()
+    today_date = date.today()
+    for i in tqdm(range(len(nifty_50_stocks))):
+        file = open(f'daily_analysis/nifty_stocks/{today_date}.txt', 'a')
+        retry_count = 10
         got_data = False
         retry_num = 1
         while (not got_data) and (retry_num < retry_count):
             try:
-                data = data_agent.get_ohlc_data(i, Interval.in_weekly, 100)
+                data = data_agent.get_ohlc_data(nifty_50_stocks[i], Interval.in_weekly, 100)
                 got_data = True
             except:
-                time.sleep(20)
+                time.sleep(2)
                 retry_num += 1
 
         analysis_agent = AnalysisAgent()
@@ -24,5 +28,7 @@ if __name__=='__main__':
         nearest_sellside_price = nearest_sellside['nearest_liquidity_in_range']
 
         if nearest_sellside_price!=0:
-            print(f'Stock {i} is near its liquidity pool at price {nearest_sellside_price} current price is {data[-1]["close"]}')
-        time.sleep(20)
+            file.write(f'Stock {nifty_50_stocks[i]} is near its liquidity pool at price {nearest_sellside_price} current price is {data[-1]["close"]}\n')
+            file.close()
+            # print(f'Stock {nifty_50_stocks[i]} is near its liquidity pool at price {nearest_sellside_price} current price is {data[-1]["close"]}')
+        time.sleep(2)
