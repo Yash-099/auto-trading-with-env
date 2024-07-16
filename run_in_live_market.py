05a921ce476b8ffbf6b72b43e8a117878bc08f92
Yash-099 <ybhavsar@adobe.com>
2024-07-17 01:04:58 +0530

    try running on github server

---
+++ run_in_live_market.py
@@ -0,0 +1,18 @@
+import time
+from datetime import datetime
+
+def is_within_hours():
+    now = datetime.now()
+    start_time = now.replace(hour=9, minute=15, second=0, microsecond=0)
+    end_time = now.replace(hour=15, minute=30, second=0, microsecond=0)
+
+    return start_time <= now <= end_time
+
+while True:
+    if is_within_hours():
+        # Place your code here
+        print("Running script...")
+    else:
+        print("Outside working hours. Waiting...")
+
+    time.sleep(300)  # Check every 5 minutes
